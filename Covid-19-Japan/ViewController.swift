a28e91c9d24e0d0ff4fdf6e12d631fac743b641c
KaitoMuraoka <kaitoboshi@icloud.com>
2021-12-30 08:21:22 +0900

    first commit

---
+++ Covid-19-Japan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid-19-Japan
+//
+//  Created by 村岡海人 on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
